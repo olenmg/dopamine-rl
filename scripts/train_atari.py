246b0c895cfa84bdd054f13fe1c97ed51db8a33b
olenmg <jgheon.kim@gmail.com>
2023-10-23 15:35:30 +0900

    fix: atari script alignment

--- scripts/train_atari.py
+++ scripts/train_atari.py
@@ -10,19 +10,19 @@ from render import render
 
 ENVS = [
     "ALE/Alien-v5", "ALE/Amidar-v5", "ALE/BeamRider-v5",
-    "ALE/DemonAttack-v5", "ALE/Pong-v5", "ALE/SpaceInvaders-v5",
-    "ALE/Tutankham-v5", "ALE/Zaxxon-v5", "ALE/Breakout-v5",
+    "ALE/Breakout-v5", "ALE/DemonAttack-v5", "ALE/IceHockey-v5",
+    "ALE/Pong-v5", "ALE/RoadRunner-v5", "ALE/SpaceInvaders-v5",
+    "ALE/Tutankham-v5", "ALE/Venture-v5", "ALE/Zaxxon-v5",
+    "ALE/Kangaroo-v5", "ALE/Qbert-v5", "ALE/Seaquest-v5", 
     "ALE/VideoPinball-v5", "ALE/Boxing-v5", "ALE/StarGunner-v5", 
     "ALE/Robotank-v5", "ALE/Atlantis-v5", "ALE/CrazyClimber-v5", 
     "ALE/Gopher-v5", "ALE/NameThisGame-v5", "ALE/Krull-v5", 
-    "ALE/Assault-v5", "ALE/RoadRunner-v5", "ALE/Kangaroo-v5", 
-    "ALE/Jamesbond-v5", "ALE/Tennis-v5", "ALE/KungFuMaster-v5", 
-    "ALE/Freeway-v5", "ALE/TimePilot-v5", "ALE/Enduro-v5",
-    "ALE/FishingDerby-v5", "ALE/UpNDown-v5", "ALE/IceHockey-v5",
-    "ALE/Qbert-v5", "ALE/Hero-v5", "ALE/Asterix-v5", 
-    "ALE/BattleZone-v5", "ALE/WizardOfWor-v5", "ALE/ChopperCommand-v5", 
-    "ALE/Centipede-v5", "ALE/BankHeist-v5", "ALE/Riverraid-v5", 
-    "ALE/Venture-v5", "ALE/Seaquest-v5", "ALE/DoubleDunk-v5", 
+    "ALE/Assault-v5", "ALE/Jamesbond-v5", "ALE/Tennis-v5",
+    "ALE/KungFuMaster-v5", "ALE/Freeway-v5", "ALE/TimePilot-v5", 
+    "ALE/Enduro-v5", "ALE/FishingDerby-v5", "ALE/UpNDown-v5", 
+    "ALE/Hero-v5", "ALE/Asterix-v5", "ALE/BattleZone-v5",
+    "ALE/WizardOfWor-v5", "ALE/ChopperCommand-v5", "ALE/Centipede-v5",
+    "ALE/BankHeist-v5", "ALE/Riverraid-v5", "ALE/DoubleDunk-v5", 
     "ALE/Bowling-v5", "ALE/MsPacman-v5", "ALE/Asteroids-v5", 
     "ALE/Frostbite-v5", "ALE/Gravitar-v5", "ALE/PrivateEye-v5", 
     "ALE/MontezumaRevenge-v5"
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         help="Algorithm"
     )
     parser.add_argument(
-        '--run_name', type=str,
+        '--run_name', type=str, default=None,
         help="Run name (name of log)"
     )
     parser.add_argument(
@@ -60,7 +60,8 @@ if __name__ == "__main__":
             train_config = TrainConfig(**json.load(f))
         with open(os.path.join("configs/algo_configs", args.cfg), "r") as f:
             algo_config = ALGO_CONFIG[train_config.algo](**json.load(f))
-        train_config.run_name = args.log_path = f"{args.algo}-{env_id[4:-3]}-{args.run_name}"
+        train_config.run_name = args.log_path = f"{args.algo}-{env_id[4:-3]}" + \
+            "" if args.run_name is None else f"-{args.run_name}"
         train_config.env_id = env_id
 
         print(train_config.__dict__)
